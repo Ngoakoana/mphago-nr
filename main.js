fa452f33235656b87fd3b2339e48885fdcfccb67
Ngoakoana <ngoakoana.mphago460@gmail.com>
2024-06-03 09:45:35 +0200

    added new files

---
+++ main.js
@@ -0,0 +1,27 @@
+let num1 =prompt('enter your number');
+
+let num2 = prompt('enter your number');
+let op = prompt('enter oparator')
+if (op==='+'){ 
+    console(num1 + num2);}
+    else if (op==='-'){
+        console(num1-num2)
+    }
+    else if (op ==='/')[
+        console(num1/num2)
+    ]
+    else if (op=== '*'){
+        console (num1*num2)
+    }
+    else console("invalid operate")
+    
+
+
+
+
+
+
+
+
+
+
