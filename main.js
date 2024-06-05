344b8f790d6bd144c0ce358643cb52841947a76e
Ngoakoana <ngoakoana.mphago460@gmail.com>
2024-06-05 10:59:33 +0200

    button

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-let num1 =prompt('enter your number');
+// let num1 =prompt('enter your number');
 
-let num2 = prompt('enter your number');
-let op = prompt('enter oparator')
-if (op==='+'){ 
-    console(num1 + num2);}
-    else if (op==='-'){
-        console(num1-num2)
-    }
-    else if (op ==='/')[
-        console(num1/num2)
-    ]
-    else if (op=== '*'){
-        console (num1*num2)
-    }
-    else console("invalid operate")
+// let num2 = prompt('enter your number');
+// let op = prompt('enter oparator')
+// if (op==='+'){ 
+//     console(num1 + num2);}
+//     else if (op==='-'){
+//         console(num1-num2)
+//     }
+//     else if (op ==='/')[
+//         console(num1/num2)
+//     ]
+//     else if (op=== '*'){
+//         console (num1*num2)
+//     }
+//     else console("invalid operate")
     
+let showQty  =prompt ('display all item number');
+buttononclick= alert('your Qty is'+'qty');
+buttononclick= Qty=Qty+1;
+buttononclick= Qty=Qty+2;
+buttonclick= Qty =Qty +3;
+alert('your Qty'+'Qty');
+buttononclick= Qty=0;
+(yourQty+Qty);
+
+ 
+
+
+
+
+
+
+
+
 
 
 
